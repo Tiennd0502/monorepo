6b2586e10ff9a21cf6a903ea8d7bfc9ae3b6aeb9
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-03-19 17:18:47 +0700

    Implement Text component

---
+++ monorepo/libs/ui/src/compoments/Text/index.tsx
@@ -0,0 +1,67 @@
+import { Text as TextBase, styled } from 'tamagui';
+
+const Text = styled(TextBase, {
+  color: '$textDefault',
+  fontWeight: 'normal',
+  fontFamily: '$body',
+  allowFontScaling: false,
+
+  defaultVariants: {
+    size: 'medium',
+  },
+
+  variants: {
+    error: {
+      true: {
+        color: '$errorPrimary',
+      },
+    },
+    bold: {
+      true: {
+        fontWeight: 'bold',
+        fontFamily: '$title',
+      },
+    },
+    boldReversal: {
+      true: {
+        fontFamily: '$heading',
+        color: '$textReversal',
+        fontWeight: 'bold',
+      },
+    },
+
+    size: {
+      small: {
+        fontSize: '$2.5',
+        lineHeight: 12,
+      },
+
+      medium: {
+        fontSize: '$3.5',
+        lineHeight: 20,
+      },
+
+      large: {
+        fontSize: '$4',
+        lineHeight: 20,
+      },
+
+      extraMedium: {
+        fontSize: '$5',
+        lineHeight: 20,
+      },
+
+      extraLarge: {
+        fontSize: '$6',
+        lineHeight: 30,
+      },
+
+      huge: {
+        fontSize: '$7.5',
+        lineHeight: '30',
+      },
+    },
+  } as const,
+});
+
+export default Text;
--- monorepo/libs/ui/src/themes/colors.ts
+++ monorepo/libs/ui/src/themes/colors.ts
@@ -3,6 +3,60 @@ const colors = {
   secondary: '#fff',
 } as const;
 
+const backgroundColors = {
+  backgroundPrimary: colors.primary,
+  backgroundSecondary: colors.secondary,
+  backgroundInputDisabled: '#909090',
+  backgroundIcon: '#fff',
+  backgroundIconDisabled: '#e6e6e6',
+} as const;
+
+const textColors = {
+  textPrimary: colors.primary,
+  textSecondary: colors.secondary,
+  textDefault: '#999',
+  textTertiary: '#909090',
+  textBold: '#000000',
+  textLink: colors.primary,
+} as const;
+
+const buttonColors = {
+  buttonPrimary: colors.primary,
+  buttonGhost: 'transparent',
+  buttonDisable: '#909090',
+};
+
+const statusColors = {
+  successPrimary: '#27ae60',
+  errorPrimary: '#eb5757',
+  warningPrimary: '#f2c94c',
+} as const;
+
+const borderColor = {
+  borderPrimary: '#dbdbdb',
+  borderSecondary: '#bdbdbd',
+  boderTertiary: '#8a959e33',
+} as const;
+
+const boxShadowColors = {
+  boxShadowPrimary: colors.primary,
+  boxShadowSecondary: 'rgba(52, 52, 52, 0.8)',
+  boxShadowTertiary: '#999',
+} as const;
+
+const iconColors = {
+  iconNormal: colors.primary,
+  iconActived: '#333',
+  iconDisabled: '#909090',
+} as const;
+
 export const colorsMapping = {
   ...colors,
+  ...textColors,
+  ...backgroundColors,
+  ...statusColors,
+  ...boxShadowColors,
+  ...borderColor,
+  ...iconColors,
+  ...buttonColors,
 };

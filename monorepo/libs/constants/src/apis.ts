2e7485ce478a70b41727818ee93db266b36b6e5a
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-03-25 09:08:45 +0700

    Add package for api

---
+++ monorepo/libs/constants/src/apis.ts
@@ -0,0 +1,19 @@
+export const API_PATH = {
+  USERS: 'v1/users',
+  LOGIN: 'v1/users/login',
+  LOGOUT: 'v1/users/logout',
+  REGISTER: 'v1/users/register',
+  VERIFY: 'v1/users/verify',
+  PRODUCT: 'products/v1/listings',
+  CATEGORIES: 'v1/categories',
+  ADDRESSES: 'v1/addresses',
+  CART: 'products/v1/cart',
+  // Payments
+  LAYERS: 'v1/layers',
+  PAYMENT_METHOD: 'v1/tenants/payment_methods',
+  CHECKOUT: 'products/v1/cart/checkout',
+  ORDERS: 'products/v1/orders',
+  REVIEWS: 'v1/reviews',
+};
+
+export const AUTH_STORE_KEY = 'auth-storage';

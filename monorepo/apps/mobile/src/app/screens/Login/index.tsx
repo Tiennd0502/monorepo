21ca79c6a1f9240ce4cec8115c7b865fa0831c5b
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-03-25 13:14:47 +0700

    Implement Login screen

--- monorepo/apps/mobile/src/app/screens/Login/index.tsx
+++ monorepo/apps/mobile/src/app/screens/Login/index.tsx
@@ -1,13 +1,223 @@
-import React from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
-import { Text } from '@monorepo/ui';
-import { View } from 'react-native';
+// Types
+import {
+  AUTH_TYPES,
+  AuthResponse,
+  LoginForm,
+  LoginPayLoad,
+} from '@monorepo/types';
+import { SCREENS } from '../../types';
 
-const Login = () => {
+// Constants
+import { SCHEMA } from '@monorepo/constants';
+
+// Utils
+import { getRandomId } from '@monorepo/utils';
+
+// Hooks | Stores
+import { useAuth } from '@monorepo/hooks';
+import { authStore, userStore } from '@monorepo/stores';
+
+// Components
+import { Stack, ScrollView, XStack, YStack } from 'tamagui';
+import {
+  Button,
+  Divider,
+  HiddenIcon,
+  IconButton,
+  Input,
+  Loading,
+  LogoIcon,
+  ShowIcon,
+  Text,
+} from '@monorepo/ui';
+
+const Login = ({ navigation }) => {
+  const [isDisclosure, setIsDisclosure] = useState(true);
+  const [setAuthKey] = authStore((state) => [state.setAuthKey]);
+  const [setUser] = userStore((state) => [state.setUser]);
+
+  const {
+    logIn: { mutate, isPending },
+  } = useAuth();
+
+  const {
+    control,
+    handleSubmit,
+    reset,
+    clearErrors,
+    formState: { isValid },
+  } = useForm<LoginForm>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+  });
+
+  const inputs = useMemo(
+    () => [
+      {
+        name: 'email',
+        label: 'Email',
+        rules: SCHEMA.email,
+      },
+      {
+        name: 'password',
+        label: 'Password',
+        rules: SCHEMA.password,
+        secureTextEntry: isDisclosure,
+        rightElement: {
+          icon: (
+            <IconButton onPress={() => setIsDisclosure((prev) => !prev)}>
+              {isDisclosure ? <ShowIcon /> : <HiddenIcon />}
+            </IconButton>
+          ),
+        },
+      },
+    ],
+    [isDisclosure]
+  );
+
+  const handleLogin = useCallback(
+    ({ email, password }: LoginForm) => {
+      const payload: LoginPayLoad = {
+        user: {
+          uuid: getRandomId(),
+          email,
+          password,
+          type: AUTH_TYPES.CUSTOMER,
+        },
+      };
+
+      mutate(payload, {
+        onSuccess: ({ status, data: { user } }: AuthResponse) => {
+          const { key } = user;
+          key && setAuthKey(key);
+          user && setUser(user);
+          status && navigation.navigate(SCREENS.MAIN_STACK);
+          reset();
+        },
+        onError: (error: Error) => {
+          console.log(error);
+        },
+      });
+    },
+    [mutate, navigation, reset, setAuthKey, setUser]
+  );
+
+  const handleSignUp = useCallback(() => {
+    navigation.navigate(SCREENS.SIGN_UP);
+    reset();
+    clearErrors();
+  }, [clearErrors, navigation, reset]);
+
   return (
-    <View style={{ flex: 1 }}>
-      <Text>Login Page</Text>
-    </View>
+    <>
+      {isPending && <Loading />}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Stack paddingVertical={30} paddingRight={30}>
+          <Stack paddingLeft={30} paddingBottom={30}>
+            <XStack
+              justifyContent="space-between"
+              alignItems="center"
+              alignContent="center"
+              gap={20}
+            >
+              <YStack flexGrow={1}>
+                <Divider color="$borderSecondary" />
+              </YStack>
+              <YStack>
+                <LogoIcon />
+              </YStack>
+              <YStack flexGrow={1}>
+                <Divider color="$borderSecondary" />
+              </YStack>
+            </XStack>
+          </Stack>
+          <Text
+            size="extraLarge"
+            fontWeight="bold"
+            color="$textTertiary"
+            marginBottom="$2"
+            paddingLeft="$6"
+          >
+            Hello !
+          </Text>
+          <Text
+            size="extraLarge"
+            color="$primary"
+            fontWeight="bold"
+            marginBottom="$6"
+            paddingLeft="$6"
+          >
+            WELCOME BACK
+          </Text>
+          <Stack
+            backgroundColor="$secondary"
+            overflow="hidden"
+            paddingLeft="$2"
+            paddingTop="$4"
+          >
+            {inputs.map(
+              ({ name, label, rules, rightElement, secureTextEntry }) => (
+                <>
+                  <Controller
+                    name={name}
+                    control={control}
+                    rules={rules}
+                    render={({
+                      field: { onChange, ...props },
+                      fieldState: { error },
+                    }) => {
+                      return (
+                        <Input
+                          variant="flushed"
+                          label={label}
+                          placeholder={label}
+                          errorMessage={error?.message}
+                          onChangeText={onChange}
+                          rightElement={rightElement}
+                          secureTextEntry={secureTextEntry}
+                          {...props}
+                        />
+                      );
+                    }}
+                  />
+                  <Divider
+                    color="$borderSecondary"
+                    height="$0.5"
+                    marginLeft="$4"
+                    marginBottom="$3"
+                  />
+                </>
+              )
+            )}
+
+            <Stack
+              paddingTop={25}
+              rowGap={25}
+              paddingRight={30}
+              paddingLeft={10}
+            >
+              <Button variant="chromeless">Forgot Password</Button>
+              <Button
+                disabled={!isValid || isPending}
+                onPress={handleSubmit(handleLogin)}
+              >
+                Log in
+              </Button>
+              <Button variant="chromeless" onPress={handleSignUp}>
+                SIGN UP
+              </Button>
+            </Stack>
+          </Stack>
+        </Stack>
+      </ScrollView>
+    </>
   );
 };
 

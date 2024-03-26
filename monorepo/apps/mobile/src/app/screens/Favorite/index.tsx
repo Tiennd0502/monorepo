57c6a7a7c8392bea6653fd620531aea21d84d6fc
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-03-26 16:23:54 +0700

    Implement Favorite screen

--- monorepo/apps/mobile/src/app/screens/Favorite/index.tsx
+++ monorepo/apps/mobile/src/app/screens/Favorite/index.tsx
@@ -1,9 +1,80 @@
-import React from 'react';
+import { Stack, ScrollView } from 'tamagui';
+import { Fragment, useCallback, useMemo, useState } from 'react';
 
-import { Text } from '@monorepo/ui';
+// Types
+import { SCREENS } from '../../types';
+import { ProductResponse } from '@monorepo/types';
 
-const Favorite = () => {
-  return <Text>Favorite Page</Text>;
+// Constants
+import { INIT_PAGE } from '@monorepo/constants';
+
+// Utils
+import { formatProducts, getData } from '@monorepo/utils';
+
+// Hooks | Stores
+import { useProducts } from '@monorepo/hooks';
+
+// Themes | Components
+import {
+  Button,
+  CartIcon,
+  SearchIcon,
+  CartItem,
+  Divider,
+  Header,
+  Loading,
+} from '@monorepo/ui';
+
+const Favorite = ({ navigation }) => {
+  const { useFetchProducts } = useProducts();
+  const { data, isPending } = useFetchProducts(INIT_PAGE);
+  const pages = useMemo(() => data?.pages || [], [data?.pages]);
+  const products = useMemo(
+    () => (pages.length > 0 && getData(pages)) || [],
+    [pages]
+  );
+
+  const productsFormat = useMemo(
+    () =>
+      (products.length > 0 && formatProducts(products as ProductResponse[])) ||
+      [],
+    [products]
+  );
+  const [carts, setCarts] = useState(productsFormat);
+
+  const handleViewCart = () => navigation.navigate(SCREENS.CART);
+
+  const handleDeleteItem = useCallback((id: string) => {
+    setCarts((prev) => prev.filter((item) => item.id !== id));
+  }, []);
+
+  const handleAddToCart = useCallback(() => null, []);
+
+  return (
+    <Stack flex={1} paddingHorizontal={20} backgroundColor="$secondary">
+      <Stack paddingVertical="$3.75">
+        <Header
+          title="Favorites"
+          startIcon={<SearchIcon />}
+          endIcon={<CartIcon onPress={handleViewCart} />}
+        />
+      </Stack>
+      {isPending && <Loading />}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        {carts.map((item, index) => (
+          <Fragment key={index}>
+            <CartItem isFavorites item={item} onDelete={handleDeleteItem} />
+            {index < carts.length - 1 && (
+              <Divider color="$backgroundTertiary" />
+            )}
+          </Fragment>
+        ))}
+      </ScrollView>
+      <Button marginBottom="$5" onPress={handleAddToCart}>
+        Add all to my cart
+      </Button>
+    </Stack>
+  );
 };
 
 export default Favorite;

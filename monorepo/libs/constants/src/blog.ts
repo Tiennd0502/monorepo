135a432c1bdba3f7488d6d3fb0d551f6f6268203
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-03-27 11:02:45 +0700

    Implement Notification screen

--- monorepo/libs/constants/src/blog.ts
+++ monorepo/libs/constants/src/blog.ts
@@ -3,14 +3,7 @@ import { NEW_STATUS } from '@monorepo/types';
 export const NOTIFICATIONS = [
   {
     id: '01',
-    title: 'Hello world!',
-    description:
-      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
-    status: NEW_STATUS.NORMAL,
-  },
-  {
-    id: '02',
-    title: 'Hello world!',
+    title: 'Your order #123456789 has been confirmed',
     description:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
     status: NEW_STATUS.NEW,
@@ -18,11 +11,51 @@ export const NOTIFICATIONS = [
       'https://i.ibb.co/m8nVWx2/sarah-dorweiler-fr0-J5-GIVyg-unsplash-1.png',
   },
   {
+    id: '02',
+    title: 'Your order #123456789 has been canceled',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
+    status: NEW_STATUS.NORMAL,
+    image:
+      'https://i.ibb.co/m8nVWx2/sarah-dorweiler-fr0-J5-GIVyg-unsplash-1.png',
+  },
+  {
     id: '03',
-    title: 'Hello world!',
+    title: 'Discover hot sale furnitures this week.',
     description:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
     status: NEW_STATUS.HOT,
+  },
+  {
+    id: '04',
+    title: 'Your order #123456789 has been shipped successfully',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
+    status: NEW_STATUS.NORMAL,
+    image: 'https://i.ibb.co/KwRDcc4/3968901-1.png',
+  },
+  {
+    id: '05',
+    title: 'Your order #123456789 has been confirmed',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
+    status: NEW_STATUS.NORMAL,
+    image: 'https://i.ibb.co/KwRDcc4/3968901-1.png',
+  },
+  {
+    id: '06',
+    title: 'Your order #123456789 has been canceled',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
+    status: NEW_STATUS.NORMAL,
+    image: 'https://i.ibb.co/KwRDcc4/3968901-1.png',
+  },
+  {
+    id: '07',
+    title: 'Your order #123456789 has been shipped successfully',
+    description:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Reiciendis possimus quod explicabo maiores repudiandae laudantium quam provident consectetur in dolores.',
+    status: NEW_STATUS.NORMAL,
     image: 'https://i.ibb.co/KwRDcc4/3968901-1.png',
   },
 ];

439092a63c2d03c95c47452f5d39e5765cdebefa
Tien Nguyen <tien.nguyen@asnet.com.vn>
2024-03-28 16:26:24 +0700

    Implement BlogCard component

--- monorepo/libs/constants/src/blog.ts
+++ monorepo/libs/constants/src/blog.ts
@@ -1,5 +1,50 @@
 import { NEW_STATUS } from '@monorepo/types';
 
+export const BLOGS = [
+  {
+    id: '01',
+    title: 'Product 01',
+    image:
+      'https://i.ibb.co/m8nVWx2/sarah-dorweiler-fr0-J5-GIVyg-unsplash-1.png',
+    price: '30.00',
+    evaluate: 5,
+    createdAt: '20/03/2020',
+    description:
+      'Nice Furniture with good delivery.Nice Furniture with good delivery. The delivery time is very fast. Then products look like exactly the picture in the app. Besides, color is also the same and quality is very good despite very cheap price',
+  },
+  {
+    id: '02',
+    title: 'Product 02',
+    image: 'https://i.ibb.co/KwRDcc4/3968901-1.png',
+    price: '34.00',
+    evaluate: 2,
+    createdAt: '20/03/2020',
+    description:
+      'Nice Furniture with good delivery.Nice Furniture with good delivery. The delivery time is very fast. Then products look like exactly the picture in the app. Besides, color is also the same and quality is very good despite very cheap price',
+  },
+  {
+    id: '03',
+    title: 'Product 03',
+    image:
+      'https://i.ibb.co/MDpMXMG/jonny-caspari-wsv-CC6-Uy-Kjs-unsplash-1.png',
+    price: '20.00',
+    evaluate: 1.4,
+    createdAt: '20/03/2020',
+    description:
+      'Nice Furniture with good delivery.Nice Furniture with good delivery. The delivery time is very fast. Then products look like exactly the picture in the app. Besides, color is also the same and quality is very good despite very cheap price',
+  },
+  {
+    id: '04',
+    title: 'Product 01',
+    image: 'https://i.ibb.co/y85nTTn/2681826-1.png',
+    price: '30.00',
+    evaluate: 5,
+    createdAt: '20/03/2020',
+    description:
+      'Nice Furniture with good delivery.Nice Furniture with good delivery. The delivery time is very fast. Then products look like exactly the picture in the app. Besides, color is also the same and quality is very good despite very cheap price',
+  },
+];
+
 export const NOTIFICATIONS = [
   {
     id: '01',
---
+++ monorepo/libs/ui/src/compoments/BlogCard/BlogCard.stories.tsx
@@ -0,0 +1,37 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Stack } from 'tamagui';
+
+import { BLOGS } from '@monorepo/constants';
+
+import BlogCard from '.';
+
+const meta: Meta<typeof BlogCard> = {
+  title: 'Components/BlogCard',
+  component: BlogCard,
+  decorators: [
+    (Story) => (
+      <Stack width={450} paddingTop={20}>
+        <Story />
+      </Stack>
+    ),
+  ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof BlogCard>;
+
+export const Default: Story = {
+  args: {
+    item: BLOGS[0],
+    onPress: () => null,
+  },
+};
+
+export const Favorite: Story = {
+  args: {
+    isReview: true,
+    item: BLOGS[0],
+    onPress: () => null,
+  },
+};
